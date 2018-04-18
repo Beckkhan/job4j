889a624a17a3ba14098373ab44a8ab6b72897e46
Beckkhan <beckkhan@mail.ru>
2018-04-18 10:36:55 +0300

    4. Использование Git в IDEA

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,10 @@
 /**
-* Package for calculate task.
-*
-* @author Vyacheslav Khan (beckkhan@mail.ru)
-* @version $Id$
-* @since 10.04.2018
-*/
+ * Package for calculate task.
+ *
+ * @author Vyacheslav Khan (beckkhan@mail.ru)
+ * @version $Id$
+ * @since 10.04.2018
+ */
 
 package ru.job4j.calculate;
 
@@ -13,18 +13,17 @@ public class Calculate {
 	 * Method main.
 	 * @param args - args.
 	 */
-	 
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-	
+
 	/**
-     * Method echo.
-     * @param name Vyacheslav Khan.
-     * @return Echo plus your name.
-     */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
-    }
-	
+	 * Method echo.
+	 * @param name Vyacheslav Khan.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -7,23 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Vyacheslav Khan (beckkhan@mail.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Vyacheslav Khan (beckkhan@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Vyacheslav Khan";
-    String expect = "Echo, echo, echo : Vyacheslav Khan"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Vyacheslav Khan";
+        String expect = "Echo, echo, echo : Vyacheslav Khan";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
