a4e7fd37674b58fed12ed48d52446033620c0006
Beckkhan <beckkhan@mail.ru>
2018-04-27 13:03:50 +0300

    5.4. Двухмерный массив. Таблица умножения.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class BubbleSort {
     public int[] sort(int[] array) {
-        for (int i = array.length - 1; i>0; i--) {
+        for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (array[j + 1] < array[j]) {
                     int tmp = array[j];
