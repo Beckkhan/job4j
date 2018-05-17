f0b4b1bbb8a97a207201163d3a8397e2b6d94575
Beckkhan <beckkhan@mail.ru>
2018-05-17 15:34:28 +0300

    Создание классов профессий.

---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,23 @@
+package ru.job4j.profession;
+
+public class Profession {
+	
+	public String name;
+	public String specialization;
+	
+	public Profession() {
+	}
+	
+	public Profession(String name, String specialization) {
+		this.name = name;
+		this.specialization = specialization;
+	}
+	
+	public String getname() {
+		return this.name;
+	}
+	
+	public String getSpecialization() {
+		return this.name;
+	}
+}
