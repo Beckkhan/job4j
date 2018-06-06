216f60f75c816a5a0a7e3012a9225ba54a21e525
Beckkhan <beckkhan@mail.ru>
2018-06-06 23:16:18 +0300

    4. Использование шаблона проектирования - "Стратегия" [#786]

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -0,0 +1,13 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Vyacheslav Khan (mailto:beckkhan@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Paint {
+	public void draw(Shape shape) {
+		System.out.println(shape.draw());
+	}
+}
---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
@@ -0,0 +1,11 @@
+package ru.job4j.pseudo;
+
+/**
+ * @author Vyacheslav Khan (mailto:beckkhan@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public interface Shape {
+    String draw();
+}
