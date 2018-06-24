b0d83f088ad564d02380dba2921c8f5115851f0f
Beckkhan <beckkhan@mail.ru>
2018-06-24 22:05:35 +0300

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/start/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/start/ConsoleInput.java
@@ -20,11 +20,10 @@ public class ConsoleInput implements Input {
 				break;
 			}
 		}
-		if (exist) {
-			return key;
-		} else {
-			throw new MenuOutException("Out of menu range. ");
+		if (!exist) {
+		    throw new MenuOutException("Out of menu range. ");
 		}
+		return key;
 	}
-	
+
 }
