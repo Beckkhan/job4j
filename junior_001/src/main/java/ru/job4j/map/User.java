3082ec22b74c30566cc337b30b47c3991f3646df
Beckkhan <beckkhan@mail.ru>
2018-10-18 14:01:44 +0300

    1. Create model of User. [#999].

---
+++ junior_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,47 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+/**
+ * @author Vyacheslav Khan (mailto: beckkhan@mail.ru)
+ * @version 1.0
+ * @since 18.10.2018
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                children == user.children &&
+                Objects.equals(birthday, user.birthday);
+    }
+}
---
+++ junior_001/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Vyacheslav Khan (mailto: beckkhan@mail.ru)
+ * @version 1.0
+ * @since 18.10.2018
+ */
+public class UserTest {
+    Calendar birthday = new GregorianCalendar(2018,10,18);
+    private User first = new User("Kolobok", 3, birthday);
+    private User second = new User("Kolobok", 3, birthday);
+
+    @Test
+    public void whenCreateTwoEqualUsersAndAddToHashSetThenCheck() {
+        Set<User> users = new HashSet<>();
+        users.add(first);
+        users.add(second);
+        System.out.println(users);
+        assertThat(users.size(),is(1));
+    }
+}
