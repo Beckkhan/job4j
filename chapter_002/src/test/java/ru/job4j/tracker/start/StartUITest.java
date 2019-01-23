ec4fdd9b121697cb00ace9c71d7925d5b3b5b999
Beckkhan <beckkhan@mail.ru>
2019-01-23 12:58:35 +0300

    2. In the class StartUI replace the output in the console on the Consumer. [#24255] Fixed.

--- chapter_002/src/test/java/ru/job4j/tracker/start/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/start/StartUITest.java
@@ -28,6 +28,20 @@ public class StartUITest {
             stdout.println(s);
         }
     };
+    private final static String MENU = new StringBuilder()
+            .append("0: Add the new item.")
+            .append(System.lineSeparator())
+            .append("1: Show all items.")
+            .append(System.lineSeparator())
+            .append("2: Edit item.")
+            .append(System.lineSeparator())
+            .append("3: Delete item.")
+            .append(System.lineSeparator())
+            .append("4: Find item by id.")
+            .append(System.lineSeparator())
+            .append("5: Find item by name.")
+            .append(System.lineSeparator())
+            .toString();
     /*@Before
     public void loadOutput() {
         System.out.println("execute before method");
@@ -47,7 +61,6 @@ public class StartUITest {
         new StartUI(input, tracker, output).init();     //   создаём StartUI и вызываем метод init()
         assertThat(tracker.getAll().get(0).getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
     }
-
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
         // создаём Tracker
@@ -61,7 +74,6 @@ public class StartUITest {
         // проверяем, что отредактированный элемент массива в трекере содержит имя, введённое при эмуляции.
         assertThat(tracker.findById(item.getId()).getName(), is("test name"));
     }
-
     @Test
     public void whenDeleteThenTrackerHasDeletedValue() {
         Tracker tracker = new Tracker();
@@ -70,7 +82,6 @@ public class StartUITest {
         new StartUI(input, tracker, output).init();
         assertNull(tracker.findById(item.getId()));
     }
-
     @Test
     public  void whenTrackerShowAllItems() {
         Tracker tracker = new Tracker();
@@ -82,33 +93,18 @@ public class StartUITest {
         tracker.add(three);
         Input input = new StubInput(new String[]{"1", "y"});
         new StartUI(input, tracker, output).init();
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("0: Add the new item.")
-                                .append(System.lineSeparator())
-                                .append("1: Show all items.")
-                                .append(System.lineSeparator())
-                                .append("2: Edit item.")
-                                .append(System.lineSeparator())
-                                .append("3: Delete item.")
-                                .append(System.lineSeparator())
-                                .append("4: Find item by id.")
-                                .append(System.lineSeparator())
-                                .append("5: Find item by name.")
-                                .append(System.lineSeparator())
-                                .append(one.getId() + ". " + one.getName())
-                                .append(System.lineSeparator())
-                                .append(two.getId() + ". " + two.getName())
-                                .append(System.lineSeparator())
-                                .append(three.getId() + ". " + three.getName())
-                                .append(System.lineSeparator())
-                                .toString()
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                        .append(MENU)
+                        .append(one.getId() + ". " + one.getName())
+                        .append(System.lineSeparator())
+                        .append(two.getId() + ". " + two.getName())
+                        .append(System.lineSeparator())
+                        .append(three.getId() + ". " + three.getName())
+                        .append(System.lineSeparator())
+                        .toString()
                 )
         );
     }
-
     @Test
     public  void whenUserFindByItemByIdThenTrackerFindByItemById() {
         Tracker tracker = new Tracker();
@@ -120,27 +116,13 @@ public class StartUITest {
         tracker.add(third);
         Input input = new StubInput(new String[]{"4", second.getId(), "y"});
         new StartUI(input, tracker, output).init();
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("0: Add the new item.")
-                                .append(System.lineSeparator())
-                                .append("1: Show all items.")
-                                .append(System.lineSeparator())
-                                .append("2: Edit item.")
-                                .append(System.lineSeparator())
-                                .append("3: Delete item.")
-                                .append(System.lineSeparator())
-                                .append("4: Find item by id.")
-                                .append(System.lineSeparator())
-                                .append("5: Find item by name.")
-                                .append(System.lineSeparator())
-                                .append("Found item with id: ")
-                                .append(second.getId() + " and name: ")
-                                .append(second.getName())
-                                .append(System.lineSeparator())
-                                .toString()
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                        .append(MENU)
+                        .append("Found item with id: ")
+                        .append(second.getId() + " and name: ")
+                        .append(second.getName())
+                        .append(System.lineSeparator())
+                        .toString()
                 )
         );
     }
@@ -155,27 +137,13 @@ public class StartUITest {
         tracker.add(three);
         Input input = new StubInput(new String[]{"5", three.getName(), "y"});
         new StartUI(input, tracker, output).init();
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("0: Add the new item.")
-                                .append(System.lineSeparator())
-                                .append("1: Show all items.")
-                                .append(System.lineSeparator())
-                                .append("2: Edit item.")
-                                .append(System.lineSeparator())
-                                .append("3: Delete item.")
-                                .append(System.lineSeparator())
-                                .append("4: Find item by id.")
-                                .append(System.lineSeparator())
-                                .append("5: Find item by name.")
-                                .append(System.lineSeparator())
-                                .append("Found item with name: ")
-                                .append(three.getName() + " and id: ")
-                                .append(three.getId())
-                                .append(System.lineSeparator())
-                                .toString()
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                        .append(MENU)
+                        .append("Found item with name: ")
+                        .append(three.getName() + " and id: ")
+                        .append(three.getId())
+                        .append(System.lineSeparator())
+                        .toString()
                 )
         );
     }
@@ -185,24 +153,6 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Input input = new StubInput(new String[]{"0", "newtestname", "newtestdescription", "y"});
         new StartUI(input, tracker, output).init();
-        assertThat(
-                new String(out.toByteArray()),
-                is(
-                        new StringBuilder()
-                                .append("0: Add the new item.")
-                                .append(System.lineSeparator())
-                                .append("1: Show all items.")
-                                .append(System.lineSeparator())
-                                .append("2: Edit item.")
-                                .append(System.lineSeparator())
-                                .append("3: Delete item.")
-                                .append(System.lineSeparator())
-                                .append("4: Find item by id.")
-                                .append(System.lineSeparator())
-                                .append("5: Find item by name.")
-                                .append(System.lineSeparator())
-                                .toString()
-                )
-        );
+        assertThat(new String(out.toByteArray()), is(MENU));
     }
 }
