ad55714026797e22a2c58051682ede3f63f1af1f
Beckkhan <beckkhan@mail.ru>
2019-01-30 14:26:38 +0300

    1. Collection API improvements. Part 1. [#70623]

--- chapter_003/src/test/java/ru/job4j/comparator/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/comparator/SortUserTest.java
@@ -9,50 +9,28 @@ import java.util.*;
  * @since 29.08.2018
  */
 public class SortUserTest {
+    SortUser sortUser = new SortUser();
+    User first = new User("Fido", 15);
+    User second = new User("Tornvald", 33);
+    User third = new User("Groom", 29);
+    User fourth = new User("Silor", 37);
+    List<User> users = List.of(first, second, third, fourth);
     @Test
     public void whenSortUserTest() {
-        SortUser sortUser = new SortUser();
-        List<User> users = new ArrayList<>();
-        users.add(new User("Fido", 15));
-        users.add(new User("Tornvald", 33));
-        users.add(new User("Groom", 29));
-        users.add(new User("Silor", 37));
         Set<User> result = sortUser.sort(users);
         assertThat(result.iterator().next().getAge(), is(15));
     }
     @Test
     public void whenSortByNameLenght() {
-        SortUser sortUser = new SortUser();
-        List<User> users = new ArrayList<>();
-        users.add(new User("Fido", 15));
-        users.add(new User("Tornvald", 33));
-        users.add(new User("Groom", 29));
-        users.add(new User("Silor", 37));
         SortUser.NameLenghtCompare comp = new SortUser.NameLenghtCompare();
         List<User> result = sortUser.sortNameLength(users, comp);
         assertThat(result.iterator().next().getName(), is("Fido"));
-        System.out.println(result.toString());
     }
     @Test
     public void whenSortByAllFields() {
-        SortUser sortUser = new SortUser();
-        List<User> users = new ArrayList<>();
-        User first = new User("Сергей", 25);
-        User second = new User("Иван", 30);
-        User third = new User("Сергей", 20);
-        User fourth = new User("Иван", 25);
-        users.add(first);
-        users.add(second);
-        users.add(third);
-        users.add(fourth);
         SortUser.AllFieldsCompare afcomp = new SortUser.AllFieldsCompare();
         List<User> result = sortUser.sortByAllFields(users, afcomp);
-        List<User> expect = new ArrayList<>();
-        expect.add(fourth);
-        expect.add(second);
-        expect.add(third);
-        expect.add(first);
-        System.out.println(result.toString());
+        List<User> expect = List.of(first, third, fourth, second);
         assertThat(result, is(expect));
     }
 }
