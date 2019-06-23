e3a07c3437b34c2c2b0ba632e01140d8a78ea690
Beckkhan <beckkhan@mail.ru>
2019-06-23 21:16:35 +0300

    1. Crud servlet, Web app architecture. Fixed. [#2512]

--- middle_002/src/main/java/ru/job4j/http/UserServlet.java
+++ middle_002/src/main/java/ru/job4j/http/UserServlet.java
@@ -4,19 +4,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
 /**
  * @author Khan Vyacheslav (mailto: beckkhan@mail.ru)
- * @version 3.0
- * @since 22.06.2019
+ * @version 4.0
+ * @since 23.06.2019
  */
 public class UserServlet extends HttpServlet {
-    /**
-     * Validate instance.
-     */
+
     private final Validate logic = ValidateService.getInstance();
 
     private final Map<String, Function<User, String>> actions = new HashMap<>();
@@ -29,6 +29,9 @@ public class UserServlet extends HttpServlet {
 
     private User buildUser(HttpServletRequest req) {
         String id = req.getParameter("id");
+        if (id == null) {
+            id = "";
+        }
         String name = req.getParameter("name");
         String login = req.getParameter("login");
         String email = req.getParameter("email");
@@ -37,16 +40,27 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.sendRedirect(String.format("%s/index.jsp", req.getContextPath()));
-    }
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        List<User> users = this.logic.findAll();
+        if (users.size() == 0) {
+            writer.append("Storage is empty.");
+        } else {
+            users.forEach(user -> {
+                writer.append(user.toString() + System.lineSeparator());
+            });
+        }
+        writer.flush();   }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String action = req.getParameter("action");
         if (actions.containsKey(action)) {
             User user = buildUser(req);
-            actions.get(action).apply(user);
-            resp.sendRedirect(String.format("%s/index.jsp", req.getContextPath()));
+            resp.setContentType("text/html");
+            PrintWriter writer = new PrintWriter(resp.getOutputStream());
+            writer.append(actions.get(action).apply(user));
+            writer.flush();
         } else {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
